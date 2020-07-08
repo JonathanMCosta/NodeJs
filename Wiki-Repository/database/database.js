c0f14a2cbd0cea358aadf0c5998ffc8a6587607b
Jonathan Marques da Costa Franco <54005646+JonathanMCosta@users.noreply.github.com>
2020-07-08 15:26:18 -0300

    creadenciais

--- Wiki-Repository/database/database.js
+++ Wiki-Repository/database/database.js
@@ -1,7 +1,7 @@
 const Sequelize = require("sequelize");
 
-const connection = new Sequelize("blog", "gsoft_MySql", "Ms@402507", {
-  host: "128.201.72.240",
+const connection = new Sequelize("suaBaseDeDados", "seuUsuario", "suaSenha", {
+  host: "seuIP",
   dialect: "mysql",
   timezone: "-03:00",
 });
